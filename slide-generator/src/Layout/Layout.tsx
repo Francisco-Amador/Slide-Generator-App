2eec92eb2f92c4743b7bc0273d6ccf11e9d60fab
JesusAbarcaRodriguez <121053502+JesusAbarcaRodriguez@users.noreply.github.com>
2023-06-12 20:30:06 -0600

    [IMPLEMENT tailwind header-footer]
    The layout and styles of the header and footer components

--- slide-generator/src/Layout/Layout.tsx
+++ slide-generator/src/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/root/components/Footer/Footer";
 import NavBar from "@/root/components/NavBar/NavBar";
+import ProductInfo from "@/root/components/product-info/Product-Info";
 import { ReactNode } from "react";
 
 type LayoutProps = {
@@ -9,6 +10,7 @@ const Layout = ({ children }: LayoutProps) => {
     return (
         <>
             <NavBar/>
+            <ProductInfo/>
             <main className="flex justify-center items-center bg-slate-200">{children}</main>
             <Footer></Footer>
         </>
--- slide-generator/src/root/components/Footer/Footer.tsx
+++ slide-generator/src/root/components/Footer/Footer.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 import Image from "next/image";
 export default function Footer () {
     return(
-        <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+        <footer className="bg-black  shadow dark:bg-gray-900">
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
             <div  className="flex items-center mb-4 sm:mb-0">
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Slide-Generator</span>
             </div>
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
