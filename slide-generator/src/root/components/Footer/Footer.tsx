bd76a4ab499ed9a702ae31e38c0ce574ae84c3f3
JesusAbarcaRodriguez <121053502+JesusAbarcaRodriguez@users.noreply.github.com>
2023-06-16 16:10:35 -0600

    [INSTALL framer-motion and react-modal]
    The framer-motion will be used to give more life to the design and the modal so that the images look larger when selected.

--- slide-generator/src/root/components/Footer/Footer.tsx
+++ slide-generator/src/root/components/Footer/Footer.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link";
-import Image from "next/image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 export default function Footer() {
     return (
-        <footer className="bg-gray-800 shadow dark:bg-gray-900">
+        <footer className="bg-gray-600 shadow dark:bg-gray-900">
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <div className="flex items-center mb-4 sm:mb-0">
@@ -10,13 +13,17 @@ export default function Footer() {
                     </div>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-white sm:mb-0 dark:text-gray-400">
                         <li>
-                            <Link href="#" className="mr-4 hover:underline md:mr-6 ">Contact</Link>
+                            <Link href="#" className="mr-4 hover:underline md:mr-6 "><FontAwesomeIcon icon={faEnvelope} /></Link>
                         </li>
                         <li>
-                            <Link href="#" className="mr-4 hover:underline md:mr-6">Facebook</Link>
+                            <Link href="https://www.facebook.com/login/" className="mr-4 hover:underline md:mr-6">
+                                <FontAwesomeIcon icon={faFacebook} />
+                            </Link>
                         </li>
                         <li>
-                            <Link href="#" className="mr-4 hover:underline md:mr-6 ">Instagram</Link>
+                            <Link href="https://www.instagram.com" className="mr-4 hover:underline md:mr-6 ">
+                            <FontAwesomeIcon icon={faInstagram} />
+                            </Link>
                         </li>
                     </ul>
                 </div>
